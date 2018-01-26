0290647563acb8d2fc104940da923b594def0a4f
Dmitriy Haidashenko <d.s.gaydashenko@gmail.com>
2018-01-26 18:01:04 +0200

    Allow for sale to accept several assets

--- src/transactions/dex/DeleteSaleParticipationOpFrame.cpp
+++ src/transactions/dex/DeleteSaleParticipationOpFrame.cpp
@@ -8,6 +8,8 @@
 #include "ledger/SaleHelper.h"
 #include "main/Application.h"
 #include "ledger/OfferHelper.h"
+#include "CreateSaleParticipationOpFrame.h"
+#include "ledger/BalanceHelper.h"
 
 namespace stellar
 {
@@ -54,13 +56,14 @@ bool DeleteSaleParticipationOpFrame::doApply(Application& app,
         return false;
     }
 
-    if (sale->getState(ledgerManager.getCloseTime()) != SaleFrame::State::ACTIVE)
+    if (CreateSaleParticipationOpFrame::getSaleState(sale, db, ledgerManager.getCloseTime()) != SaleFrame::State::ACTIVE)
     {
         innerResult().code(ManageOfferResultCode::SALE_IS_NOT_ACTIVE);
         return false;
     }
 
-    sale->subCurrentCap(offer->getOffer().quoteAmount);
+    auto balance = BalanceHelper::Instance()->mustLoadBalance(mManageOffer.quoteBalance, db);
+    sale->subCurrentCap(balance->getAsset(), offer->getOffer().quoteAmount);
     SaleHelper::Instance()->storeChange(delta, db, sale->mEntry);
     return DeleteOfferOpFrame::doApply(app, delta, ledgerManager);
 }

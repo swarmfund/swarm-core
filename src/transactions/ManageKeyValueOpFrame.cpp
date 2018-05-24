ad4dc82b7e0a1410e08c96e6f59e1291842db3c6
Artem <artemkaskriabin@gmail.com>
2018-05-24 16:56:10 +0300

    Add should use to doApply method

--- src/transactions/ManageKeyValueOpFrame.cpp
+++ src/transactions/ManageKeyValueOpFrame.cpp
@@ -54,7 +54,10 @@ namespace stellar {
             return true;
         }
 
-        keyValueFrame->mEntry.data.keyValue().value = mManageKeyValue.action.value().value;
+        if (ledgerManager.shouldUse(LedgerVersion::KEY_VALUE_UPDATE))
+        {
+            keyValueFrame->mEntry.data.keyValue().value = mManageKeyValue.action.value().value;
+        }
         keyValueHelper->storeChange(delta, db, keyValueFrame->mEntry);
 
         return true;

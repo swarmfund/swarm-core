7608e55b4832065f252d79f323f046403a34164a
Andriy Kunitsyn <me@kunitsyn.me>
2018-07-27 15:57:02 +0300

    Add first mock

---
+++ src/transactions/test/unit/BindExternalSystemAccountIdUnitTests.cpp
@@ -0,0 +1,57 @@
+#include "bucket/BucketManager.h"
+#include "herder/Herder.h"
+#include "invariant/Invariants.h"
+#include "main/CommandHandler.h"
+#include "main/PersistentState.h"
+#include "medida/metrics_registry.h"
+#include "overlay/BanManager.h"
+#include "overlay/OverlayManager.h"
+#include "process/ProcessManager.h"
+#include "simulation/LoadGenerator.h"
+#include "test/test_marshaler.h"
+#include "transactions/BindExternalSystemAccountIdOpFrame.h"
+#include "transactions/test/mocks/MockApplication.h"
+#include "util/StatusManager.h"
+#include "util/Timer.h"
+#include "util/TmpDir.h"
+#include "work/WorkManager.h"
+
+using namespace stellar;
+typedef std::unique_ptr<Application> appPtr;
+
+TEST_CASE("bind external system account_id - unit test",
+          "[tx][bind_external_system_account_id_unit_test]")
+{
+    /*
+    MockOperation operationMock;
+    MockOperationFrame operationFrameMock;
+    MockTransactionFrame parentTxFrameMock;
+    MockApplication appMock;
+    MockLedgerDelta ledgerDeltaMock;
+    MockLedgerManager ledgerManagerMock;
+
+    SECTION("Happy path")
+    {
+        BindExternalSystemAccountIdOpFrame opFrame(operationMock,
+    operationFrameMock, parentTxFrameMock);
+
+        EXPECT_CALL( ... );
+        opFrame.doApply(appMock, ledgerDeltaMock, ledgerManagerMock);
+    }
+
+    SECTION("Checking validity of valid frame")
+    {
+        BindExternalSystemAccountIdOpFrame opFrame(operationMock,
+    operationFrameMock, parentTxFrameMock);
+        EXCEPT_TRUE(opFrame.doCheckValid(appMock));
+    }
+
+    SECTION("Checking validity of invalid frame")
+    {
+        BindExternalSystemAccountIdOpFrame opFrame(operationMock,
+    operationFrameMock, parentTxFrameMock);
+        EXCEPT_FALSE(opFrame.doCheckValid(appMock));
+    }
+        */
+    MockApplication mockApp;
+}

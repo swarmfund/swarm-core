8f431667cf892b3066ec470e92fe803a0ebd13b9
Yevhenii Luhanskyi <y.luhanskyi@gmail.com>
2018-08-07 17:47:31 +0300

    Refactor test helpers for being able to test txs with multiple ops

--- src/transactions/test/test_helper/TxHelper.cpp
+++ src/transactions/test/test_helper/TxHelper.cpp
@@ -32,6 +32,43 @@ namespace txtest
 		return res;
 	}
 
+    TransactionFramePtr TxHelper::txFromOperations(Account& source, std::vector<Operation>& ops, uint64_t* maxTotalFee,
+                                                   Account* signer)
+    {
+        Transaction tx;
+        tx.sourceAccount = source.key.getPublicKey();
+        tx.salt = source.getNextSalt();
+        tx.timeBounds.minTime = 0;
+        tx.timeBounds.maxTime = INT64_MAX / 2;
+
+        if (maxTotalFee != nullptr)
+        {
+            tx.ext.v(LedgerVersion::ADD_TRANSACTION_FEE);
+            tx.ext.maxTotalFee() = *maxTotalFee;
+        }
+
+        for (auto& op : ops)
+        {
+            if (tx.operations.size() == 100)
+            {
+                break;
+            }
+
+            tx.operations.push_back(op);
+        }
+
+        TransactionEnvelope envelope;
+        envelope.tx = tx;
+        auto res = TransactionFrame::makeTransactionFromWire(mTestManager->getNetworkID(), envelope);
+
+        if (signer == nullptr) {
+            signer = &source;
+        }
+        res->addSignature(signer->key);
+
+        return res;
+    }
+
 	TxHelper::TxHelper(TestManager::pointer testManager)
 	{
 		mTestManager = testManager;

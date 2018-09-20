38ca505f0df2b02341e5831699856de8e53f0941
Skriabin <artemkaskriabin@gmail.com>
2018-09-20 13:59:26 +0300

    Add some payout unit tests

--- src/transactions/test/unit/PayoutUnitTests.cpp
+++ src/transactions/test/unit/PayoutUnitTests.cpp
@@ -1,6 +1,3 @@
-/*
-#include <transactions/test/mocks/MockBalanceHelper.h>
-#include <transactions/test/mocks/MockAssetHelper.h>
 #include "bucket/BucketManager.h"
 #include "herder/Herder.h"
 #include "invariant/Invariants.h"
@@ -17,6 +14,8 @@
 #include "transactions/test/mocks/MockApplication.h"
 #include "transactions/test/mocks/MockDatabase.h"
 #include "transactions/test/mocks/MockKeyValueHelper.h"
+#include "transactions/test/mocks/MockBalanceHelper.h"
+#include "transactions/test/mocks/MockAssetHelper.h"
 #include "transactions/test/mocks/MockLedgerDelta.h"
 #include "transactions/test/mocks/MockLedgerManager.h"
 #include "transactions/test/mocks/MockSignatureValidator.h"
@@ -33,12 +32,8 @@
 using namespace stellar;
 using namespace testing;
 
-static uint256 sourceAccountPublicKey = hexToBin256(
-        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABB");
-
 TEST_CASE("payout - unit test", "[tx][payout]")
 {
-
     MockApplication appMock;
     MockLedgerManager ledgerManagerMock;
     MockTransactionFrame transactionFrameMock;
@@ -60,18 +55,28 @@ TEST_CASE("payout - unit test", "[tx][payout]")
     op.minPayoutAmount = ONE;
     op.asset = "LTC";
     auto balance = SecretKey::random();
+    auto source = txtest::Account{SecretKey::random(), Salt(0)};
+    auto sourceID = source.key.getPublicKey();
     op.sourceBalanceID = balance.getPublicKey();
     Operation operation;
     operation.body = Operation::_body_t(OperationType::PAYOUT);
     operation.body.payoutOp() = op;
-    operation.sourceAccount =
-            xdr::pointer<AccountID>(new AccountID(CryptoKeyType::KEY_TYPE_ED25519));
-    operation.sourceAccount->ed25519() = sourceAccountPublicKey;
+    operation.sourceAccount.activate() = sourceID;
     OperationResult operationResult;
+    operationResult.code(OperationResultCode::opINNER);
+    operationResult.tr().type(OperationType::PAYOUT);
 
     AccountFrame::pointer accountFrameFake =
             AccountFrame::makeAuthOnlyAccount(*operation.sourceAccount);
     LedgerHeader ledgerHeaderFake;
+    AssetCreationRequest request;
+    request.code = op.asset;
+    request.preissuedAssetSigner = sourceID;
+    request.initialPreissuedAmount = ONE;
+    request.maxIssuanceAmount = ONE;
+    request.policies = static_cast<uint32_t>(AssetPolicy ::TRANSFERABLE);
+    request.details =  "";
+    AssetFrame::pointer assetFrameFake = AssetFrame::create(request, sourceID);
 
     ON_CALL(appMock, getDatabase()).WillByDefault(ReturnRef(dbMock));
     ON_CALL(appMock, getLedgerManager())
@@ -100,7 +105,7 @@ TEST_CASE("payout - unit test", "[tx][payout]")
     {
         EXPECT_CALL(transactionFrameMock,
                     loadAccount(&ledgerDeltaMock, Ref(dbMock),
-                                *operation.sourceAccount))
+                                sourceID))
                 .WillOnce(Return(accountFrameFake));
         EXPECT_CALL(appMock,
                     getMetrics())
@@ -108,8 +113,41 @@ TEST_CASE("payout - unit test", "[tx][payout]")
         REQUIRE_FALSE(opFrame.checkValid(appMock, &ledgerDeltaMock));
     }
 
-    SECTION("Apply, no pool entry to bind")
+    SECTION("Apply success do check valid")
     {
+        REQUIRE(opFrame.doCheckValid(appMock));
+        REQUIRE(opFrame.getResult().code() == OperationResultCode::opINNER);
+        REQUIRE(opFrame.getResult().tr().payoutResult().code() ==
+                PayoutResultCode::SUCCESS);
+    }
+
+    SECTION("Apply, no such asset")
+    {
+        REQUIRE_FALSE(
+                opFrame.doApply(appMock, storageHelperMock, ledgerManagerMock));
+        REQUIRE(opFrame.getResult().code() ==
+                OperationResultCode::opINNER);
+        REQUIRE(opFrame.getResult().tr().payoutResult().code() ==
+                PayoutResultCode::ASSET_NOT_FOUND);
+    }
+
+    SECTION("Apply, asset not transferable")
+    {
+        assetFrameFake->setPolicies(0);
+        EXPECT_CALL(assetHelperMock, loadAsset(op.asset, sourceID))
+                .WillOnce(Return(assetFrameFake));
+        REQUIRE_FALSE(
+                opFrame.doApply(appMock, storageHelperMock, ledgerManagerMock));
+        REQUIRE(opFrame.getResult().code() ==
+                OperationResultCode::opINNER);
+        REQUIRE(opFrame.getResult().tr().payoutResult().code() ==
+                PayoutResultCode::ASSET_NOT_TRANSFERABLE);
+    }
+
+    SECTION("Apply, no such balance")
+    {
+        EXPECT_CALL(assetHelperMock, loadAsset(op.asset, sourceID))
+                .WillOnce(Return(assetFrameFake));
         REQUIRE_FALSE(
                 opFrame.doApply(appMock, storageHelperMock, ledgerManagerMock));
         REQUIRE(opFrame.getResult().code() ==
@@ -117,5 +155,4 @@ TEST_CASE("payout - unit test", "[tx][payout]")
         REQUIRE(opFrame.getResult().tr().payoutResult().code() ==
                 PayoutResultCode::BALANCE_NOT_FOUND);
     }
-
-}*/
+}
